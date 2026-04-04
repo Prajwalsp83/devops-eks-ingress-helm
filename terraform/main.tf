0017a1326661e07eb0d597ec7036ebf81f9e5151
Prajwalsp83 <97027168+Prajwalsp83@users.noreply.github.com>
2026-04-04 23:49:56 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,25 @@
 provider "aws" {
-region = "ap-south-1"
+  region = "ap-south-1"
 }
 
 module "eks" {
-source  = "terraform-aws-modules/eks/aws"
-version = "~> 20.0"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
 
-cluster_name    = "devops-cluster"
-cluster_version = "1.28"
-ami_type = "AL2_x86_64"
+  cluster_name    = "devops-cluster"
+  cluster_version = "1.28"
 
-vpc_id     = "vpc-04d954b1c1bf4c257"
-subnet_ids = [
-"subnet-0de9d1596b0103733",
-"subnet-0d1f12312f2ef3149"
-]
+  vpc_id     = "vpc-04d954b1c1bf4c257"
+  subnet_ids = [
+    "subnet-0de9d1596b0103733",
+    "subnet-0d1f12312f2ef3149"
+  ]
 
-eks_managed_node_groups = {
-devops = {
-desired_size   = 2
-instance_types = ["t3.medium"]
+  eks_managed_node_groups = {
+    devops = {
+      desired_size   = 2
+      instance_types = ["t3.medium"]
+      ami_type       = "AL2023_x86_64_STANDARD"
+    }
+  }
 }
-}
-}
-
