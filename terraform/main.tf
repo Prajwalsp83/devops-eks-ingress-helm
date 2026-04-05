b4514f2262e7dcf43a731b53c603dd6230a4b2a0
Prajwalsp83 <97027168+Prajwalsp83@users.noreply.github.com>
2026-04-05 16:32:14 +0530

    Upgrade EKS cluster version and add configurations
    Upgraded EKS cluster version from 1.28 to 1.31 and added encryption and logging configurations.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket         = "devops-terraform-state-psp"
-    key            = "eks/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-lock"
-  }
-}
-
-provider "aws" {
-  region = "ap-south-1"
-}
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
   cluster_name    = "devops-cluster"
-  cluster_version = "1.28"
+  cluster_version = "1.31"  # upgrade from 1.28
 
   vpc_id     = "vpc-04d954b1c1bf4c257"
   subnet_ids = [
@@ -24,9 +11,17 @@ module "eks" {
     "subnet-0d1f12312f2ef3149"
   ]
 
+  # Dev/non-prod: encryption and logs disabled
+  cluster_encryption_config    = {}
+  create_kms_key               = false
+  cluster_enabled_log_types    = []
+  create_cloudwatch_log_group  = false
+
   eks_managed_node_groups = {
     devops = {
       desired_size   = 2
+      min_size       = 1
+      max_size       = 4
       instance_types = ["t3.medium"]
       ami_type       = "AL2_x86_64"
     }
