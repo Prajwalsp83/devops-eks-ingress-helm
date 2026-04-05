6a45e66938987982a970e010f02367b443f10527
Prajwalsp83 <97027168+Prajwalsp83@users.noreply.github.com>
2026-04-05 13:51:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("DevOps App Running 🚀 Version 1");
+  res.send("PRAJWAL SP  🚀 Version 1");
 });
 
 app.listen(3000, () => {
